1fd489340927ca72fa6356e6ace19e35e6f5b80d
Gregory Gapienko <skorpion1813@yandex.ru>
2023-08-12 23:19:13 +0300

    update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,37 +1,24 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
+import {
+  greeting, getRandom, gameLauncher,
+} from '../src/index.js';
 
-const userName = readlineSync.question('May I have your name? ');
-let calcCorrect = 0;
-const needForWin = 3;
-
-function greeting() {
-  const hello = `Hello, ${userName}!`;
-  const gameConditions = 'Answer "yes" if the number is even, otherwise answer "no".';
-  return `${hello}\n${gameConditions}`;
-}
+const gameConditions = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 function isEven(number) {
   return number % 2 === 0;
 }
 
-function game() {
-  const questionNumber = Math.floor(Math.random() * 100);
+function gameIsEven() {
+  const questionNumber = getRandom(100);
   const question = `Question: ${questionNumber}`;
-  const answer = readlineSync.question(`${question}\nYour answer: `);
-  if (
-    (isEven(questionNumber) && answer === 'yes')
-    || (!isEven(questionNumber) && answer === 'no')
-  ) {
-    console.log('Correct!');
-    calcCorrect += 1;
-    return calcCorrect === needForWin
-      ? `Congratulations, ${userName}!`
-      : game();
-  }
-  return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}`;
+  const correctAnswer = isEven(questionNumber) ? 'yes' : 'no';
+  const userAnswer = readlineSync.question(`${question}\nYour answer: `);
+  return [userAnswer, correctAnswer];
 }
 
 console.log(greeting());
-console.log(game());
+console.log(gameConditions);
+console.log(gameLauncher(0, gameIsEven));
