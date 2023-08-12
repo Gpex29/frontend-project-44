d2f7a2b6cecfc718dba3946e864bb7db990c4cc0
Gregory Gapienko <skorpion1813@yandex.ru>
2023-08-12 23:21:22 +0300

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+const needForWin = 3;
+
+function greeting() {
+  return `Hello, ${userName}!`;
+}
+
+function getRandom(range) {
+  return Math.floor(Math.random() * range);
+}
+
+function getWrong(userAnswer, correctAnswer) {
+  return `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}`;
+}
+
+function gameLauncher(correctCount, game) {
+  if (correctCount === needForWin) {
+    console.log(`Congratulations, ${userName}!`);
+    return;
+  }
+  const [userAnswer, correctAnswer] = game();
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    gameLauncher(correctCount + 1, game);
+  } else {
+    console.log(getWrong(userAnswer, correctAnswer));
+  }
+}
+
+export {
+  userName, needForWin, greeting, getRandom, gameLauncher,
+};
